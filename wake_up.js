2c28df30f5a413a6f210f6ed0861627e3b20fc38
a17822228533-hash <a17822228533@icloud.com>
2026-08-08 15:02:36 +0800

    Update wake_up.js
    1

--- wake_up.js
+++ wake_up.js
@@ -40,7 +40,7 @@ function getLastUserTime(messages) {
     }
   }
   try { return fs.statSync(TIMELINE_PATH).mtime; } catch (_) {}
-  return new Date(0);
+  return new Date(）;
 }
 
 async function runWakeUp() {
